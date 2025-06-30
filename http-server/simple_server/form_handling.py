f000ee3e5a7bc57d5205de008b68f637e04eb430
jayeshhire <jayeshhire96@gmail.com>
2025-06-30 22:59:54 +0530

    imported the form_handling and test_form_handling files from the dev branch

--- http-server/simple_server/form_handling.py
+++ http-server/simple_server/form_handling.py
@@ -1,5 +1,6 @@
 import re
 from abc import ABC, abstractmethod
+import json
 
 
 class UnsupportedMediaTypeException(Exception):
@@ -30,7 +31,7 @@ class FormAbstract(ABC):
         pass 
 
     @abstractmethod
-    def get_form_data(self):
+    def parse_data(self):
         pass 
 
 
@@ -52,7 +53,7 @@ class URLEncodedForm(FormAbstract):
             self.extract_header()
 
             if self.validate_content_type():
-                self.get_form_data()
+                self.parse_data()
             else:
                 raise UnsupportedMediaTypeException("Only url encoded form data can be handled.")
 
@@ -87,10 +88,10 @@ class URLEncodedForm(FormAbstract):
             else:
                 return False
         
-        def get_form_data(self):
+        def parse_data(self):
             d_ = re.search("\r\n\r\n(.*)", self.request_body).groups()[0]
             if self.content_length is None:
-                raise OpSeqInvalidException("call extract_header and validate_content_type method first before calling get_form_data ")
+                raise OpSeqInvalidException("call extract_header and validate_content_type method first before calling parse_data ")
             
             if not self.validate_content_type():
                 raise UnsupportedMediaTypeException(f"Content type should be 'application/x-www-form-urlencoded' but {self.content_type} was given")
@@ -126,11 +127,11 @@ class MultipartForm(FormAbstract):
     def extract_header(self):
         header_str = self.request_body.split("\r\n\r\n")[0]
         host_pattern = r"Host:\s*(.*)\r\n"
-        content_type_pattern = r"Content-Type:\s*(.*)\r\n"
-        boundary_pattern = r';boundary="(.*)";|\r\n'
-        content_type_val = re.search(host_pattern, self.request_body, re.IGNORECASE).groups()[0]
+        content_type_pattern = r"Content-Type:\s*(.*)"
+        boundary_pattern = r';boundary="(.*)"'
+        content_type_val = re.search(content_type_pattern, header_str, re.IGNORECASE).groups()[0]
         self.boundary = re.search(boundary_pattern, content_type_val, re.IGNORECASE).groups()[0]
-        self.host = re.search(host_pattern, self.request_body, re.IGNORECASE).groups()[0]
+        self.host = re.search(host_pattern, header_str, re.IGNORECASE).groups()[0]
         self.content_type = re.search(r"(.*);", content_type_val, re.IGNORECASE).groups()[0]
 
     def validate_content_type(self):
@@ -142,30 +143,97 @@ class MultipartForm(FormAbstract):
         else:
             return False
 
-    def get_form_data(self):
+    def parse_data(self):
         if not self.validate_content_type():
             raise UnsupportedMediaTypeException(f"content type should be 'multipart/form-data' but {self.content_type} was given")
         
-        pat = fr"\r\n(--{self.boundary}\r\n.*\r\n\r\n.*\r\n--{self.boundary}--)"
-        cd_pat = fr'--{self.boundary}\r\ncontent-disposition:\s*(.*\r\n\r\n.*)\r\n--{self.boundary}'
-        cd_val_pat = "(.*);.*"
-        cd_name_pat = '.*;name="(.*)"'
-        field_val_pat = '\r\n\r\n(.*)'
-        f_data = re.search(pat, self.request_body, re.IGNORECASE).groups()[0]
-        
-        content_dispositions = re.search(cd_pat, f_data, re.IGNORECASE).groups()
+        pat = fr"(--{self.boundary}\r\n.*\r\n\r\n.*\r\n)"
+        cd_pat = fr'--{self.boundary}\r\ncontent-disposition:\s*(.*\r\n\r\n.*)\r\n'
+        f_data = re.findall(pat, self.request_body, re.IGNORECASE)
+        content_dispositions = [
+            re.search(cd_pat, d, re.IGNORECASE).groups()[0]
+            for d in f_data
+        ]
 
-        fields = {}
+        fields = []
         for cd in content_dispositions:
-            cd_val = re.search(cd_val_pat, cd, re.IGNORECASE).groups()[0]
-            if cd_val.strip() == "form-data":
-                field_name = re.search(cd_name_pat, cd, re.IGNORECASE).groups()[0]
-                field_val = re.search(field_val_pat, cd, re.IGNORECASE).groups()[0]
-            fields[field_name] = field_val
+            val_header = cd.split("\r\n\r\n")
+            cd_lst = val_header[0].split(";")
+            dispo_val = cd_lst[0]
+            if dispo_val.strip() == "form-data":
+                a = [x for x in cd_lst if re.search(r"\s+name=(.*)", x) is None]
+                field_name = [x for x in cd_lst if re.search(r"\s+name=(.*)", x) is not None]
+                field_name = field_name[0].split("=")[1].strip('"')
+                attrs = {attr.split("=")[0].strip(): attr.split("=")[1].strip('"') for attr in a[1::] }
+                field_value = val_header[1]
+                fields.append(
+                        {
+                            "name": field_name,
+                            "value": field_value,
+                            "attrs": attrs
+                        }
+                    )
+
         self.form_data = fields
 
     def submit(self):
         self.get_request_line()
         self.extract_header()
-        self.validate_content_type()
-        self.get_form_data()
+        self.parse_data()
+
+
+class JSONDataHandling(FormAbstract):
+
+    def __init__(self, request_body: str):
+        self.request_body = request_body
+        self.method = None
+        self.path = None 
+        self.protocol = None
+        self.host = None 
+        self.content_type = None
+        self.content_length = None
+        self.json_data = None 
+        pass
+
+    def get_request_line(self):
+        l1 = self.request_body.split("\r\n")[0].split(" ")
+        self.method = l1[0]
+        self.path = l1[1]
+        self.protocol = l1[2]
+
+    def extract_header(self):
+        header_str = self.request_body.split("\r\n\r\n")[0]
+        host_pat = r"Host:\s*(.*)\r\n"
+        content_type_pattern = r"Content-Type:\s*(.*)\r\n"
+        content_length_pattern = r"Content-Length:\s*([0-9]*)"
+        self.host = re.search(host_pat, header_str, re.IGNORECASE).groups()[0]
+        self.content_type = re.search(content_type_pattern, header_str, re.IGNORECASE).groups()[0]
+        self.content_length = int(re.search(content_length_pattern, header_str, re.IGNORECASE).groups()[0])
+        pass
+
+    def validate_content_type(self):
+        if self.content_type is None:
+                raise OpSeqInvalidException("Incorrect call for validate_content_type function before extract_header function")
+        
+        if self.content_type == "application/json":
+            return True
+        else:
+            return False
+    
+    def parse_data(self):
+        if self.validate_content_type():
+            data_str = self.request_body.split("\r\n\r\n")[1]
+            self.json_data = json.loads(data_str)
+        else:
+            raise UnsupportedMediaTypeException(f"content type should be 'application/json' but {self.content_type} was given")
+
+    def submit(self):
+        self.get_request_line()
+        self.extract_header()
+        if self.content_length is None:
+                raise OpSeqInvalidException("call extract_header and validate_content_type method first before calling parse_data ")
+        
+        if self.validate_content_type():
+            self.parse_data()
+        else:
+            raise UnsupportedMediaTypeException(f"Content type should be 'application/json' but {self.content_type} was given")
