82e5c2f83ecac6b7f19fa38cfa69a4bbcc2de9cb
jayeshhire <jayeshhire96@gmail.com>
2025-06-30 22:50:34 +0530

    deleted some unwanted files

--- http-server/simple_server/http_header.py
+++ http-server/simple_server/http_header.py
@@ -1,12 +1,40 @@
 from pydantic import BaseModel
+from dataclasses import dataclass
+from enum import Enum
 import copy
 
-# class HeaderKeyVal(BaseModel):
+
+# @dataclass
 class HttpHeaderModel(BaseModel):
     method: str
     path: str 
     protocol: str 
     header_dict: dict
+
+
+class HttpHeaderType(Enum):
+    REQUEST_HEADER = 1
+    RESPONSE_HEADER = 2
+
+
+@dataclass
+class HttpRequestLine:
+    method: str
+    path: str
+    protocol: str
+
+
+@dataclass
+class HttpHeaderFieldAttr:
+    name: str
+    value: str
+
+
+@dataclass
+class HttpHeaderField:
+    field: str
+    values: list[str] | str
+    attrs: list[HttpHeaderFieldAttr] | None
 
 
 class HttpHeader:
@@ -18,87 +46,86 @@ class HttpHeader:
         - path
         - protocol
         - header_str
-        - header_dict
+        - header_deserialized
     """
-    def __init__(self, header_serialized: str | None = None, header_dict: dict | None = None):
-        self.header_str = header_serialized
-        self.header_dict = header_dict
-        self.serialize()
-        self.deserialize()
+    def __init__(self, header_serialized: str | None = None, header_type: HttpHeaderType | None = None, header_deserialized: list[HttpHeaderField] | None= None):
+        self.header_serialized = header_serialized
+        self.header_deserialized = header_deserialized
+        self.request_line = None
+        self.response_line = None
+        self.header_type = header_type
+        # self.serialize()
+        # self.deserialize()
     
     def deserialize(self) -> None:
-        if self.header_str is not None:
-            lines = self.header_str.split("\r\n")
+        if self.header_serialized is not None:
+            lines = self.header_serialized.split("\r\n")
             line1 = lines[0].split(" ")
-            self.method = line1[0].strip()
-            self.path = line1[1].strip()
-            self.protocol = line1[2].strip()
-            self.header_dict = str2dict(lines)
-            lines = "\r\n".join(lines[1::])
-            # self.header_dict = 
-        """
-        Function:
-            Name: str_dict
-            parameter: 
-                - content(list[str])
-            return:
-                - dict
-            
-            description: 
-                Takes a list of lines from the header and converts them into python dictionary.
-        """
-        def str2dict(header_lines: list[str]) -> dict:
-            content_dict = {}
-            key_val_lines = copy.deepcopy(header_lines)
-
-            if ":" not in header_lines[0]:
-                key_val_lines = key_val_lines[1::]
-            
-            key_val_lst = [[key_val.split(":")[0].strip(" "), 
-                            key_val.split(":")[1].strip(" ")] 
-                            for key_val in key_val_lines]
-            
-            for key, val in key_val_lst:
-                content_dict[key] = val
-            return content_dict
+            method = line1[0].strip()
+            path = line1[1].strip()
+            protocol = line1[2].strip()
+            self.request_line = HttpRequestLine(method, path, protocol)
+            self.header_deserialized = HttpHeader.header_keyval_deserialize(lines[1::])
         
     def serialize(self) -> None:
-        if self.header_dict is not None:
-            self.header_str = dict2str(self.header_dict)
+        if self.header_deserialized is not None:
+            # self.header_str = dict2str(self.header_dict)
+            header_serialize = ""
+            if self.header_type == HttpHeaderType.REQUEST_HEADER:
+                header_serialize += f"{self.request_line}\r\n"
+            elif self.header_type == HttpHeaderType.RESPONSE_HEADER:
+                header_serialize += f"{self.response_line}\r\n"
+            for header in self.header_deserialized:
+                attrs = [f";{attr.name}={attr.value}" for attr in header.attrs] 
+                tmp_header = f"{header.field}: {header.values}{attrs}\r\n"
+                header_serialize += tmp_header
 
-        """
-        Function: 
-            Name: dict2str
-            parameter: 
-                - header_dict(dict)
-            return:
-                - str (the output string contains \r\n at the end of every line 
-                and \r\n\r\n at the end of the header)
-            
-            description:
-                Converts the header dictionary to the header string
-        """
-
-        def dict2str(header_dict: dict) -> str:
-            header_str = ""
-            for key, val in header_dict.items():
-                header_str += f"{key}: {val}\r\n"
-
-            header_str+= "\r\n"
-            return header_str
+            self.header_serialized = header_serialize + "\r\n"
         
     @classmethod
-    def header_keyval_deserialize(cls, header_keyvals: str) -> dict:
-        keyvals_dict = {keyval.split(":")[0]: keyval.split(":")[1] for keyval in header_keyvals}
-        keyval_attrs = {key : 
-                            {
-                                "value": val.split(";")[0],
-                                "attr":{
-                                attr.split("=")[0]: attr.split("=")[1].split(",") 
-                                if "," in  attr.split("=")[1] else attr.split("=")[1]
-                                for attr in val.split(";")[1::]
-                                }    
-                            }
-                        for key, val in keyvals_dict.items()
-                        }
-        return keyval_attrs
+    def header_keyval_deserialize(cls, header_keyvals: list[str]) -> list[HttpHeaderField]:
+        keyval_deserialize = [HttpHeader.parse_header(key_val) for key_val in header_keyvals]
+        return keyval_deserialize
+    
+    @classmethod
+    def parse_header(cls, header_str: str = None):
+        # header_str = "User-Agent: Mozilla/5.0 , Google, Safari (Macintosh; Intel Mac OS X 10.9; rv:50.0) ; Gecko=20100101 ; Firefox=50.0 (hii)"
+        field = header_str.split(":")[0]
+
+        val_str = ":".join(header_str.split(":")[1::])
+        attrs = val_str.split(";")
+
+        new_attrs_lst = []
+
+        new_attr = ""
+
+        i = 0
+        while i < len(attrs):
+            if "(" in attrs[i] and ")" not in attrs[i]:
+                temp_i = i
+                new_attr += attrs[i]
+                i += 1
+                while ")" not in attrs[i] :
+                    new_attr += ";" + attrs[i]
+                    i += 1
+                new_attr += ";" + attrs[i]
+                i += 1
+                new_attrs_lst.append(new_attr)
+                new_attr = ""
+                continue 
+            new_attrs_lst.append(attrs[i])
+            i += 1
+
+        values = new_attrs_lst[0].split(",")
+        attrs = [HttpHeaderFieldAttr(attr.split("=")[0], attr.split("=")[1]) for attr in new_attrs_lst[1::]]
+        header_deserialized = HttpHeaderField(field, values, attrs)
+        return header_deserialized
+    
+    def __str__(self):
+        return f"HttpHeader (\
+            request-line: {self.request_line},\
+            response-line: {self.response_line},\
+            serialized: {self.header_serialized},\
+            deserialized: {self.header_deserialized},\
+            header-type: {self.header_type}\
+                )"
